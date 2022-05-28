6a7d6146ebe6386752cdc2f9d797e08a603d44f6
mgolshann <mgolshan.ir@gmail.com>
2022-05-28 18:12:40 +0430

    adding sign-up-routes and install package express-validator to validate incoming requests

---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response} from 'express';
+import { body, validationResult } from 'express-validator';
+
+const router = express.Router();
+
+router.post('/api/users/signup', 
+[
+    body('email')
+        .isEmail()
+        .withMessage('Provide a valid email'),
+    body('password')
+        .trim()
+        .isLength({ min: 4, max: 20 })
+        .withMessage('Password must be between 4 and 20')
+]
+,(req: Request, res: Response) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.status(400).send(errors.array());
+    }
+    
+    console.log('creating a user ...');
+    
+    res.status(200).send({});
+});
+
+export { router as SignupRouter };
